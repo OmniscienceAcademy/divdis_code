227a296b11d7c1ea31497dadff111caa32ca4712
crsegerie <charbel-raphael.segerie@hotmail.fr>
2022-09-10 12:51:14 +0200

    Small corrections

--- defaults.py
+++ defaults.py
@@ -1,14 +1,16 @@
 import os
-from pathlib import Path
-import sys
 default_user = 'cuda:2'
 project_name = 'ConceptExtrapolation'
 experiment_presets_file = 'experiment_presets.json'
 num_wb_images = 8
 num_workers = 0
 image_log_frequency = 100
-PARENT_DIR = '/'
+PARENT_DIR = 'tmp'
 DATA_PATH = os.path.join(PARENT_DIR, 'data')
 PARAM_PATH = os.path.join(PARENT_DIR, 'params')
 LOG_PATH = os.path.join(PARENT_DIR, 'logs')
 
+import os
+for directory in [DATA_PATH, PARAM_PATH, LOG_PATH]:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
---
+++ readme.md
@@ -0,0 +1,2 @@
+python run_experiment.py --data '/Users/raph/Desktop/Perso/effisc/happyFaces/HappyFaces' --loss_type 1 --smooth --labeled_scale 1 --distinct_scale 1 --class_certainty_scale 1 --lr 0.0004 --model 'PretrainedResnetClassifier(18,2)' --epochs 20 --batch_size 75 --mix_rates -1 0.3
+
